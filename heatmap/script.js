7c191147b4eca3eb1a9461edee1589d25d534ccd
Adesola Samuel <adesolasamuel2018@gmail.com>
2024-04-28 12:55:51 +0100

    FInishing Touches

--- heatmap/script.js
+++ heatmap/script.js
@@ -1,8 +1,8 @@
-// Initialize the map
+
 let map = L.map('map-container').setView([10.2794733, 9.8013358], 13); // Replace with your town's coordinates
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-// Sample tree loss data
+
 const treeLossReports = [
     { latitude: 10.2794733, longitude: 8.8013358, highHeat: true }, 
     { latitude: 10.7194733, longitude: 9.87113358, highHeat: true}, 
@@ -10,14 +10,14 @@ const treeLossReports = [
     { latitude: 10.9194733, longitude: 10.1113358, highHeat: true}, 
     { latitude: 10.5794733, longitude: 10.5013358, highHeat: true }, 
     { latitude: 10.9194733, longitude: 10.9113358, highHeat: true}, 
-    // ... Add more sample data points as needed
+
 ];
 
-// Display markers 
+
 treeLossReports.forEach(report => {
     let marker = L.marker([report.latitude, report.longitude]);
 
-    // Style marker if highHeat is true
+   
     if (report.highHeat) {
         marker.setIcon(L.icon({ 
             iconUrl: 'red-marker.png' 
--- treereport/script.js
+++ treereport/script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('tree-report');
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent default form submission for now
+    event.preventDefault(); 
     alert("Thank you! Tree report submitted for review.");
-    // In reality, you would send the form data to your backend here
+    
 });
